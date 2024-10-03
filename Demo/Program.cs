1e924a487e294b8b545c18551c54aa7506b46649
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-10-03 21:04:17 +0300

    Delegate Ex 01 & 02

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,63 @@
 ﻿namespace Demo
+
 {
+    // step 0
+    public delegate int StringFunDelegate(string str);
+    // new class [Delegate]
+    // any reference to a method that has the same signature as the delegate can be assigned to the delegate
+    // function or pointer to a function
+    // this function may be static or non static 
+    // this function may be in the same class or in another class
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate
+
+            // Delegate is a type that represents references to methods with a particular parameter list and return type.
+            // Delegate is a C# Language feature
+            // has 2 usages:
+            // 1. Pointer to a function
+            // 2. Event handling (event driving programming)
+
+            int X = 5;
+
+            //int Y = StringFunctions.GetCountOfUpperChars; // invalid
+
+            // step 1 Declare reference from delegate
+
+            StringFunDelegate fun;
+
+            // step 2 Assign the reference to a method
+
+            //fun = new StringFunDelegate(StringFunctions.GetCountOfUpperChars);
+            fun = StringFunctions.GetCountOfUpperChars;
+
+            // step 3 Call the method using the reference
+
+            int Y = fun.Invoke("Ahmed");
+
+            fun = StringFunctions.GetCountOfLowerChars;
+            // StringFunDelegate => GetCountOfLowerChars Only;
+
+            int Z = fun.Invoke("Ahmed");
+
+            int[] Numbers = { 5, 3, 7, 1, 9, 2, 8, 4, 6 };
+            SortingAlgorithms.BubbleSort(Numbers, CompareFunctions.CompareGreater);
+
+            foreach (int item in Numbers)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            SortingAlgorithms.BubbleSort(Numbers, CompareFunctions.CompareLess);
+
+            foreach (int item in Numbers)
+            {
+                System.Console.WriteLine(item);
+            }
+            #endregion
         }
     }
 }
---
+++ Demo/SortingAlgorithms.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public delegate bool CompareDelegate(int x, int y);
+    internal class SortingAlgorithms
+    {
+        public static void BubbleSort(int[] arr, CompareDelegate compareFunc  )
+        {
+            if (arr is null)
+                throw new ArgumentNullException(nameof(arr));
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    //if (arr[j] > arr[j + 1])
+                    //if (CompareFunctions.CompareGreater(arr[j], arr[j+1]))
+                    if (compareFunc.Invoke(arr[j], arr[j + 1]))
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        //public static void BubbleSortDesc(int[] arr)
+        //{
+        //    if (arr is null)
+        //        throw new ArgumentNullException(nameof(arr));
+
+        //    for (int i = 0; i < arr.Length - 1; i++)
+        //    {
+        //        for (int j = 0; j < arr.Length - i - 1; j++)
+        //        {
+        //            if (arr[j] < arr[j + 1])
+        //            {
+        //                int temp = arr[j];
+        //                arr[j] = arr[j + 1];
+        //                arr[j + 1] = temp;
+        //            }
+        //        }
+        //    }
+        //}
+
+
+
+    }
+}
