450915e2f16c4e39ce449314f648503f2480810b
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-10-06 22:48:54 +0300

    Part 02

--- Assignment/LibraryEngine.cs
+++ Assignment/LibraryEngine.cs
@@ -6,9 +6,25 @@ using System.Threading.Tasks;
 
 namespace Assignment
 {
+    public delegate string ProcessBookDelegate(Book book);
+
     internal class LibraryEngine
     {
-        public static void ProcessBooks (List<Book> books, Delegate)
+        public static void ProcessBooks(List<Book> books, ProcessBookDelegate deligate)
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine(deligate(book));
+            }
 
+        }
+
+        public static void ProcessBooksv2(List<Book> books, Func<Book, string> fbtr)
+        {
+            foreach (Book B in books)
+            {
+                Console.WriteLine(fbtr(B));
+            }
+        }
     }
 }
---
+++ Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Program
+    {
+  
+
+        static void Main(string[] args)
+        {
+            #region Book
+            //List<Book> books = new List<Book>()
+            //{
+            //    new Book("1", "C#", new[] {"Omar", "Ahmed", "Ali"}, DateTime.Now, 50),
+            //    new Book("2", "OOP", new[] {"Mohamed"}, DateTime.Now, 30),
+            //    new Book("3", "SQL", new[] {"Hamada", "Aya"}, DateTime.Now, 40)
+            //};
+
+            //// A
+            //BookDelegate title = BookFunctions.GetTitle;
+            //BookDelegate price = BookFunctions.GetPrice;
+            //BookDelegate authors = BookFunctions.GetAuthors;
+
+            //LibraryEngine.ProcessBooks(books, title);
+            //LibraryEngine.ProcessBooks(books, price);
+            //LibraryEngine.ProcessBooks(books, authors);
+
+            //// B
+            //Func<Book, string> func = BookFunctions.GetTitle;
+
+            //LibraryEngine.ProcessBooksv2(books, func);
+
+            //// C
+            //LibraryEngine.ProcessBooks(books, delegate (Book b) { return b.ISBN; });
+            //LibraryEngine.ProcessBooksv2(books, delegate (Book b) { return b.ISBN; });
+
+            //// D
+            //LibraryEngine.ProcessBooks(books, b => $"Publication Date: {b.PublicationDate}");
+            //LibraryEngine.ProcessBooksv2(books, b => $"Publication Date: {b.PublicationDate}"); 
+            #endregion
+
+        }
+    }
+}
