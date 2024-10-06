592a33ffed7b5d1f9f21869e1af10854854ffa1d
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-10-06 17:45:53 +0300

    Part 01

---
+++ Assignment/BookFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class BookFunctions
+    {
+        public static string GetTitle(Book book)
+        {
+            if (book is null)
+                return "No book found";
+
+            return $"Title: {book.Title}";
+        }
+
+        public static string GetAuthors(Book book)
+        {
+            if(book is null)
+                return "No book found";
+            else if(book.Authors.Length == 0)
+                return "No authors found";
+
+            return string.Join(", ", book.Authors);
+        }
+
+        public static string GetPrice(Book book)
+        {
+            if(book is null)
+                return "No book found";
+            
+            return $"Price: { book.Price.ToString("C")}";
+        }
+    }
+}
---
+++ Assignment/LibraryEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class LibraryEngine
+    {
+        public static void ProcessBooks (List<Book> books, Delegate)
+
+    }
+}
